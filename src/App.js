5ea414109ebc10f6ac9222243507a13cd9ee066d
Premjit Benake <premjit.benake@i-exceed.com>
2021-04-16 21:03:53 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import User from './components/user.component';
+import User from './components/user/user.component';
+import Task from './components/task/task.component';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchAllUser } from './redux/actions/userAction';
@@ -14,6 +15,7 @@ function App() {
 		<div>
 			<Switch>
 				<Route exact path="/users" component={User} />
+				<Route exact path="/tasks" component={Task} />
 			</Switch>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import Header from './components/header/header.component';
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router>
+				<Header />
 				<App />
 			</Router>
 		</Provider>
